e45a7f3a01f22bf62cfbd43773adf21cb6f11769
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-13 21:00:35 +0200

    update

--- R/tabs/tab4/ui.R
+++ R/tabs/tab4/ui.R
@@ -4,21 +4,24 @@ tab4ui <- function(id) {
   ns <- NS(id) # Remember to ns() all ids! 
   # <---
   
-fluidPage(sidebarLayout(
-  sidebarPanel(
+fluidPage(
+  fluidRow(column(4, 
     sliderInput(ns("bins"),
                 "Number of bins:",
                 min = 1,
                 max = 50,
-                value = 30)
-  ),
+                value = 30),
   
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotOutput(ns("distPlot"))
-  )
-))
-
+    plotOutput(ns("distPlot"))),
+    column(8, fluidRow(
+                column(6,
+                       HTML("<b>fdsafgdsgdsfd</b><hr/>")),
+                column(6,
+                       HTML("<b>fdsafgdsgdsfd</b><hr/>"))
+                )
+             )
+          )
+)
 }
 
 tab4ui("tab4")
