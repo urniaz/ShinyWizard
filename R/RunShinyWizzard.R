7801441c732d7dc346d5d620a7f6ce1b9a0df502
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-17 18:37:05 +0200

    update

--- R/RunShinyWizzard.R
+++ R/RunShinyWizzard.R
@@ -11,14 +11,14 @@ RunShinyWizzard <- function(loc = ""){
   file.copy(system.file("source", package = "ShinyWizzard"), TempPath, recursive=TRUE)
   
   # Copy project file if any 
-  if (dir.exists(loc)){
-    
-    # Clear demo data
-    file.remove(paste0(TempPath, "/core/tabs/"))
-    # Copy 
-    file.copy(loc, paste0(TempPath, "/core/tabs/"), recursive=TRUE)
-    
-  }else{stop("The directory", loc ," does not exists!")}
+  if (&& (loc != "")){
+   if (dir.exists(loc)){
+     # Clear demo data
+     file.remove(paste0(TempPath, "/core/tabs/"))
+     # Copy 
+     file.copy(loc, paste0(TempPath, "/core/tabs/"), recursive=TRUE)
+   }else{stop("The directory", loc ," does not exists!")}
+  }
   
   # Run app
   shiny::runApp(paste0(TempPath, "/source"))
