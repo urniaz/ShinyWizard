6882b12744176ef679d7a43480dd8d876eb51f39
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-18 03:04:47 +0200

    update

--- inst/source/core/tabs/tab1/ui.R
+++ inst/source/core/tabs/tab1/ui.R
@@ -1,6 +1,8 @@
-# TabName: Intro
-# TabIcon: bar-chart-o
+
 
 HTML('<h1>Welcome!</h1>
       <p>If youre seeing this page, that means lncRna is installed and running. <strong>Congratulations!</strong> </p>
     ')
+
+# TabName: Intro
+# TabIcon: bar-chart-o
