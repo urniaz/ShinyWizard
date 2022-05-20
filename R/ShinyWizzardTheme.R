9f132b0f1ffa370950c93163c68bcfcd3a48ac3c
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-20 18:07:42 +0200

    update

--- R/ShinyWizzardTheme.R
+++ R/ShinyWizzardTheme.R
@@ -19,3 +19,49 @@ ShinyWizzardTheme <- function(name = ""){
        stop("The template name '", name, "' is not avaliable. Please use one of ", paste(ShinyWizzardTheme(), collapse=", "))}
    }
 }
+
+ShinyWizzardThemeSelector <- function() {
+  shiny::fixedPanel(
+    top = "15px",
+    right = "15px",
+    draggable = TRUE,
+    style = "width: 250px; z-index: 100000;",
+    div(class = "panel panel-danger",
+        style = "box-shadow: 5px 5px 15px -5px rgba(0, 0, 0, 0.3);",
+        div(class = "panel-heading", "Select theme:"),
+        div(class = "panel-body",
+            selectInput("ShinyWizzardTheme-selector", NULL,
+                        c("default", ShinyWizzardTheme()),
+                        selectize = FALSE
+            )
+        )
+    ),
+    tags$script(
+      "$('#ShinyWizzardTheme-selector')
+  .on('change', function(el) {
+    var allThemes = $(this).find('option').map(function() {
+      if ($(this).val() === 'default')
+        return 'bootstrap';
+      else
+        return $(this).val();
+    });
+    // Find the current theme
+    var curTheme = el.target.value;
+    if (curTheme === 'default') {
+      curTheme = 'bootstrap';
+      curThemePath = 'shared/bootstrap/css/bootstrap.min.css';
+    } else {
+      curThemePath = '", system.file("templates", package = "ShinyWizard"), "/css/' + curTheme + '.min.css';
+    }
+    // Find the <link> element with that has the bootstrap.css
+    var $link = $('link').filter(function() {
+      var theme = $(this).attr('href');
+      theme = theme.replace(/^.*\\//, '').replace(/(\\.min)?\\.css$/, '');
+      return $.inArray(theme, allThemes) !== -1;
+    });
+    // Set it to the correct path
+    $link.attr('href', curThemePath);
+  });"
+    )
+  )
+}
