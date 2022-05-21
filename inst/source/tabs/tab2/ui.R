75aa927ca71f2f5f9979829c1e674ecdb378d0cc
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-22 01:17:08 +0200

    update

--- inst/source/tabs/tab2/ui.R
+++ inst/source/tabs/tab2/ui.R
@@ -3,6 +3,7 @@
 fluidPage(
   tagList(
     actionButton(inputId = ns("tab1button"), #<--- remember to ns() all IDs, the IDs corresponds to output in server file 
-                 label = "alert")
+                 label = "alert"),
+    ShinyWizzardThemeSelector()
   )
 )
