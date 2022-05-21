ec7aedd74c5c7b65c10c363ecbab88ba0fc66b0a
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-22 01:25:19 +0200

    update

--- inst/source/tabs/tab1/server.R
+++ inst/source/tabs/tab1/server.R
@@ -1,7 +1,7 @@
 # You can access the value of the widget with input$text, e.g.
-output$value <- renderPrint({ input[ns(AppTitleInput)] })
+output$value <- renderPrint({ input[ns('AppTitleInput')] })
 
-observeEvent(input[ns(AppTitleInput)], {
+observeEvent(input[ns('AppTitleInput')], {
   
   shinyjs::alert("Well done!")
   
