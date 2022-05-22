43e4c600b8920d4a94a5bbb8bde9f9915f93b8dc
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-22 04:05:54 +0200

    update

--- inst/source/core/ui/GenTitlePanel.R
+++ inst/source/core/ui/GenTitlePanel.R
@@ -4,8 +4,9 @@ GenTitlePanel <- function(){
   titlePanel(windowTitle = config$WindowTitle,
              fluidRow(
                column(11, 
-                      tags$div(id = "AppTitle", style= if (config$AppTitleVisible){}else{print("display:none;")}, config$AppTitle), tags$h5(id = "AppSubTitle", config$AppSubTitle)), 
+                      tags$div(id = "AppTitle", style= if (config$AppTitleVisible){}else{print("display:none;")}, config$AppTitle), 
+                      tags$h5(id = "AppSubTitle", style= if (config$AppSubTitleVisible){}else{print("display:none;")}, config$AppSubTitle)), 
                column(1, 
-                      img(id ="LogoFilePath", style = config$LogoStyle, src = config$LogoFilePath))
+                      img(id ="LogoFilePath", style =  if (config$LogoVisible){config$LogoStyle}else{print("display:none;")} , src = config$LogoFilePath)) 
              ))
 }
