5816cd211484db8d366f1248432a00b32ac827b0
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-23 01:13:39 +0200

    update

--- inst/source/tabs/tab2/ui.R
+++ inst/source/tabs/tab2/ui.R
@@ -7,13 +7,16 @@ HTML('<h1>Welcome!</h1>
 
 fluidPage(
   fluidRow(
+
    # AppTitleInput  
    tags$label("Application Title", class="control-label", `for`=ns("AppTitleInput")),
    tags$input(id = ns("AppTitleInput"), value = config$AppTitle, type = "text", class= "form-control input-text", onkeyup = paste0("document.getElementById('AppTitle').textContent = document.getElementById('",ns('AppTitleInput'),"').value;")),
    
    # AppSubTitle
    tags$label("Application Subtitle ", class="control-label", `for`=ns("AppSubTitle")),
-   tags$input(id = ns("AppSubTitle"), value = config$AppSubTitle, type = "text", class= "form-control input-text", onkeyup = paste0("document.getElementById('AppSubTitle').textContent = document.getElementById('",ns('AppSubTitle'),"').value;"))
+   tags$input(id = ns("AppSubTitle"), value = config$AppSubTitle, type = "text", class= "form-control input-text"),
+   
+   tags$script(paste0("document.getElementById('AppSubTitle').onkeyup = {document.getElementById('AppSubTitle').textContent = document.getElementById('",ns('AppSubTitle'),"').value;}"))
   )
 )
 
