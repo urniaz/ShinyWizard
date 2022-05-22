1e74b081812700ff0caa2d16de43014784b8a8a9
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-22 02:17:53 +0200

    op

--- inst/source/tabs/tab1/server.R
+++ inst/source/tabs/tab1/server.R
@@ -1,5 +1,4 @@
 # You can access the value of the widget with input$text, e.g.
-output$v <- renderText({ "tstasyghashfsdfsadfsadf" })
+output[ns(v)] <- renderText({ "tstasyghashfsdfsadfsadf" })
 
 print(output)
-print(input)
