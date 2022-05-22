11112b2ac1d7b14f98a656f2983b149a8810b829
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-22 15:33:13 +0200

    up

--- inst/source/tabs/tab1/ui.R
+++ inst/source/tabs/tab1/ui.R
@@ -8,8 +8,8 @@ HTML('<h1>Welcome!</h1>
 fluidPage(
   fluidRow(
    tags$div(style= "height:100px; background:red;", textOutput(ns("v"))),
-   tags$div(class="form-group shiny-input-container",
-   tags$input(id = ns("AppTitleInput"), type = "text", class= "form-control input-text", onkeyup = paste0("document.getElementById('AppTitle').textContent = document.getElementById('",ns('AppTitleInput'),"').value;"))),
+   #tags$div(class="form-group shiny-input-container",
+   tags$input(id = ns("AppTitleInput"), type = "text", class= "form-control input-text", onkeyup = paste0("document.getElementById('AppTitle').textContent = document.getElementById('",ns('AppTitleInput'),"').value;"))#),
   )
 )
 
