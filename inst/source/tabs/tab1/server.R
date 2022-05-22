a27f2f8eaae435d1b749c41c51a557665d1df817
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-22 02:05:57 +0200

    op

--- inst/source/tabs/tab1/server.R
+++ inst/source/tabs/tab1/server.R
@@ -1,5 +1,5 @@
 # You can access the value of the widget with input$text, e.g.
 output$v <- renderText({ input[ns("AppTitleInput")] })
 
-print(output[])
-print(input[])
+print(output)
+print(input)
