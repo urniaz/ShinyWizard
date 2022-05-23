98add2f5fe93c55c3e9fbfd17ba56c28f4d179fe
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-23 04:14:07 +0200

    up

--- inst/source/core/ui/GenInfoIcon.R
+++ inst/source/core/ui/GenInfoIcon.R
@@ -1,6 +1,8 @@
 #' Generate html footer on the bottom of the page
 
 GenInfoIcon <- function(){
-  HTML(paste0("<div style='position: fixed; padding: 10px 60px 10px 10px; bottom: 0; height: 30; width: 100%; background: inherited; text-align: right'><i class='fas fa-info-circle'></i></div>"))
+  actionButton(inputId = ns("tab1button"), #<--- remember to ns() all IDs, the IDs corresponds to output in server file 
+               label = "<i class='fas fa-info-circle'></i>",
+               style='position: fixed; padding: 10px 60px 10px 10px; bottom: 0; height: 30; width: 100%; background: inherited; text-align: right' )
 }
 
