aad1fa887c4338cf7d2683796dcd4eb28bf7aa8a
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-23 02:33:38 +0200

    update

--- inst/source/core/ui/GenTitlePanel.R
+++ inst/source/core/ui/GenTitlePanel.R
@@ -7,6 +7,6 @@ GenTitlePanel <- function(){
                       tags$div(id = "AppTitle", config$AppTitle), 
                       tags$h5(id = "AppSubTitle", config$AppSubTitle)), 
                column(1, 
-                      img(id ="LogoFilePath", src = config$LogoFilePath)) 
+                      img(id ="LogoFilePath", style="height: 60px; wigth:60px; float:right; vertical-align:middle", src = config$LogoFilePath)) 
              ))
 }
