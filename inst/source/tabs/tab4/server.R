ce123a45aab05515d05b89cdf0a74c65033fd340
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-23 05:39:39 +0200

    sx

--- inst/source/tabs/tab4/server.R
+++ inst/source/tabs/tab4/server.R
@@ -2,10 +2,10 @@
       count <- reactiveVal(0)
       observeEvent(input$button, {
         count(count() + 1)
+        config$AppTitle <- " change"
       })
       output$out <- renderText({
         count()
       })
       count
       
-      config$AppTitle <- " akahsajkdksdjsadasdasdas!!!"
