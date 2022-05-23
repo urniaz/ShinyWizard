a3ac9cebfce29997c65bcd4e9e6b493020b1587f
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-23 03:48:37 +0200

    update

--- inst/source/tabs/tab2/ui.R
+++ inst/source/tabs/tab2/ui.R
@@ -4,7 +4,7 @@
 
 # --- Next/Prev buttons
 # ShowButtons: TRUE
-# ButtonsAlignment: "center"
+# ButtonsAlignment: center
 
 HTML('<h1>Welcome!</h1>
       <p>If youre seeing this page, that means lncRna is installed and running. <strong>Congratulations!</strong> </p>
