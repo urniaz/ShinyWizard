b3a9bbe59b486d9fb44f2f4c0c3db5d66ddff221
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-23 04:52:23 +0200

    c

--- inst/source/core/ui/GenInfoIcon.R
+++ inst/source/core/ui/GenInfoIcon.R
@@ -4,7 +4,7 @@ GenInfoIcon <- function(id){
   fluidRow(
   actionButton(inputId = id, #<--- remember to ns() all IDs, the IDs corresponds to output in server file 
                label = HTML("<i class='fas fa-info-circle'></i>"),
-               style='position: fixed; bottom: 60; right: 60;' ),
+               style='position: fixed; bottom: 0; right: 0; margin-bottom: 125px;' ),
   
   tags$script(paste0("/*jslint browser:true*/
                document.getElementById('",id,"').addEventListener('click', function() {swal('Good job!', 'You clicked the ",id," button!', 'info');});"))
