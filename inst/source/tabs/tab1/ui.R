296326da2022b614b6367c7a3c1f43ab773c66ab
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-23 03:51:18 +0200

    update

--- inst/source/tabs/tab1/ui.R
+++ inst/source/tabs/tab1/ui.R
@@ -1,5 +1,9 @@
+# --- Navigation ---
 # TabName: Welcome
 # TabIcon: home
+
+# --- Next/Prev buttons
+# ShowButtons: FALSE
 
 HTML('<h1>Welcome!</h1>
       <p>If youre seeing this page, that means lncRna is installed and running. <strong>Congratulations!</strong> </p>
