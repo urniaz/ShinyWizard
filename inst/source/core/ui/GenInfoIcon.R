e2b871153ac225fba6a014d916e45b93011032b1
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-23 04:31:45 +0200

    update

--- inst/source/core/ui/GenInfoIcon.R
+++ inst/source/core/ui/GenInfoIcon.R
@@ -2,7 +2,7 @@
 
 GenInfoIcon <- function(id){
   actionButton(inputId = id, #<--- remember to ns() all IDs, the IDs corresponds to output in server file 
-               label = "<i class='fas fa-info-circle'></i>",
-               style='position: fixed; padding: 10px 60px 10px 10px; bottom: 50; height: 30; width: 100%; background: inherited; text-align: right' )
+               label = HTML("<i class='fas fa-info-circle'></i>"),
+               style='position: fixed; padding: 10px 160px 10px 10px; bottom: 0; background: inherited; text-align: right' )
 }
 
