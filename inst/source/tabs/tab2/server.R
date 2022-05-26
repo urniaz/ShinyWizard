d1d0cbed35495932b49fe21790f03dc195267ade
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-26 08:17:39 +0200

    s

--- inst/source/tabs/tab2/server.R
+++ inst/source/tabs/tab2/server.R
@@ -4,22 +4,22 @@
 path = readDirectoryInput(session, 'directory')
 updateDirectoryInput(session, 'directory', value = path)
 
-observeEvent(
-  ignoreNULL = TRUE,
-  eventExpr = {
-    input$directory
-  },
-  handlerExpr = {
-    if (input$directory > 0) {
-      # condition prevents handler execution on initial app launch
-      
-      # launch the directory selection dialog with initial path read from the widget
-      path = choose.dir(default = readDirectoryInput(session, 'directory'))
-      
-      # update the widget value
-      updateDirectoryInput(session, 'directory', value = path)
-    }
-  }
-)
+# observeEvent(
+#   ignoreNULL = TRUE,
+#   eventExpr = {
+#     input$directory
+#   },
+#   handlerExpr = {
+#     if (input$directory > 0) {
+#       # condition prevents handler execution on initial app launch
+#       
+#       # launch the directory selection dialog with initial path read from the widget
+#       path = choose.dir(default = readDirectoryInput(session, 'directory'))
+#       
+#       # update the widget value
+#       updateDirectoryInput(session, 'directory', value = path)
+#     }
+#   }
+# )
 
 print(input)
