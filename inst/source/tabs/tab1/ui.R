929d884549d54259be7011dd9ff1264c632266dd
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-27 01:49:47 +0200

    update

--- inst/source/tabs/tab1/ui.R
+++ inst/source/tabs/tab1/ui.R
@@ -15,9 +15,21 @@ fluidPage(
       <p>If youre seeing this page, that means ShinyWizard is installed and running. <strong>Congratulations!</strong> </p></center>
     <label for="avatar">Choose a profile picture:</label>
 
-<input type="file"
-       id="avatar" name="avatar"
-       accept="image/png, image/jpeg">'),
+<input type="file" id="FileUpload" onchange="selectFolder(event)" webkitdirectory mozdirectory msdirectory odirectory directory multiple />
+
+          <script type="text/javascript">
+function selectFolder(e) {
+    var theFiles = e.target.files;
+    var relativePath = theFiles[0].webkitRelativePath;
+    var folder = relativePath.split("/");
+    alert(folder[0]);
+}
+</script>
+
+          
+          
+          
+          '),
      verbatimTextOutput(ns("directorypath")),
     shiny::actionButton(inputId = ns("directory"),
                label = "Title")
