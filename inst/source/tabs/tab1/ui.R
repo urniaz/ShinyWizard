9943912773e55d9b781c75ee7888b317f721b518
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-05-27 02:03:19 +0200

    f

--- inst/source/tabs/tab1/ui.R
+++ inst/source/tabs/tab1/ui.R
@@ -29,7 +29,7 @@ function selectFolder(e) {
 
  <img id="output" src="" width="100" height="100">
 
-<input name="photo" type="file" accept="image/*" onchange="document.getElementById('output').src = window.URL.createObjectURL(this.files[0])">         
+<input name="photo" type="file" accept="image/*" onchange="document.getElementById("output").src = window.URL.createObjectURL(this.files[0])">         
           
           
           '),
