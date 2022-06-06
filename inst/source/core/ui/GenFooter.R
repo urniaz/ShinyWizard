014960ea701b5a0d28e96a766df2c8f28aaf7868
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-07 01:15:33 +0200

    update

--- inst/source/core/ui/GenFooter.R
+++ inst/source/core/ui/GenFooter.R
@@ -3,6 +3,6 @@
 GenFooter <- function(){
   
   # Changing this part without written permission is a violation of the license.
-  HTML(paste0("<div style='position: fixed; padding: 10px 30px 10px 10px; bottom: 0; height: 30; width: 100%; background: inherited; text-align: right'>Created with <a href='https://www.shinywizard.org'>ShinyWizard</a> © <div id ='FooterText'>", config$FooterText, "</div></div>"))
+  HTML(paste0("<div style='position: fixed; padding: 10px 30px 10px 10px; bottom: 0; height: 30; width: 100%; background: inherited; text-align: right'>Created with <a href='https://www.shinywizard.org' target='_blank'>ShinyWizard</a> © <div id ='FooterText'>", config$FooterText, "</div></div>"))
 
 }
