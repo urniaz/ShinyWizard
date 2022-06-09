d33e9d9d8f79021a782bc9cea9c932980d92c369
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-09 20:53:15 +0200

    s

--- inst/source/tabs/tab1/ui.R
+++ inst/source/tabs/tab1/ui.R
@@ -14,7 +14,13 @@ fluidPage(
      HTML('<center><h1>Welcome!</h1>
       <p>If youre seeing this page, that means ShinyWizard is installed and running. <strong>Congratulations!</strong> </p></center>
     <input type="file" id="myFile" name="filename">'),
-     actionButton(inputId = ns("butat"), label = "click")
+     actionButton(inputId = ns("butat"), label = "click"),
+     fileInput(
+       "inputId",
+       "label",
+       buttonLabel = "Browse...",
+       placeholder = "No file selected"
+     )
  )
 )
 
