8bd158bea58fc45de21932dd5a55e93dfd39ebcf
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-11 04:06:17 +0200

    d

--- inst/source/tabs/tab1/ui.R
+++ inst/source/tabs/tab1/ui.R
@@ -13,7 +13,7 @@ fluidPage(
   fluidRow(
      HTML('<center><h1>Welcome!</h1>
       <p>If youre seeing this page, that means ShinyWizard is installed and running. <strong>Congratulations!</strong> </p></center>
-            <a href="#" target="_blank" id="my-link" onclick="javascript:document.getElementById("NavBar-go_2_1").click();">Google Chrome</a>
+            <a href="#" onclick="javascript:document.getElementById("NavBar-go_2_1").click();">Google Chrome</a>
           ')
  )
 )
