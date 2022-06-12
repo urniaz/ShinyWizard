2424033bb857b51d216dd45871a75a9cf5d7f287
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-12 03:18:42 +0200

    u

--- inst/source/tabs/tab1/ui.R
+++ inst/source/tabs/tab1/ui.R
@@ -14,7 +14,8 @@ fluidPage(
      HTML('<center><h1>Welcome!</h1>
       <p>If youre seeing this page, that means ShinyWizard is installed and running. <strong>Congratulations!</strong> </p></center>
             <button id="NavBar-go_1_2" type="button" class="btn btn-default action-button shiny-bound-input">Next</button>
-          ')
+          '),
+     fileInput("projectFile", "Choose Project File", accept = ".zip")
  )
 )
 
