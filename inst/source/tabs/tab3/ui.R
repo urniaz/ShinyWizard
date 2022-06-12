cf1dcf22c7a030076ac0f9f92a186c49a8ca3b4e
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-12 13:18:26 +0200

    update

--- inst/source/tabs/tab3/ui.R
+++ inst/source/tabs/tab3/ui.R
@@ -11,8 +11,20 @@
 # InfoMessage: mes
 
 fluidPage(
-  tagList(
-    actionButton(inputId = ns("tab1button"), #<--- remember to ns() all IDs, the IDs corresponds to output in server file 
-                 label = "alert")
+  fluidRow(column(4, 
+                  sliderInput(ns("bins"),
+                              "Number of bins:",
+                              min = 1,
+                              max = 50,
+                              value = 30),
+                  
+                  plotOutput(ns("distPlot"))),
+           column(8, fluidRow(
+             column(6,
+                    HTML("<b>ui.R</b><hr/>")),
+             column(6,
+                    HTML("<b>server.R</b><hr/>"))
+           )
+           )
   )
 )
