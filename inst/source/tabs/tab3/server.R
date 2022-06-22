5622c3f04845a00292a3750547c7f2aec875c5a6
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-23 01:40:07 +0200

    f

--- inst/source/tabs/tab3/server.R
+++ inst/source/tabs/tab3/server.R
@@ -1,12 +1,42 @@
+shinyInput <- function(name, id) ns(paste(name, id, sep = "_"))
+rv <- reactiveValues(counter = 0L)
+
+observeEvent(input$add, {
+  rv$counter <- rv$counter + 1L
+  ## GO TO THE NEWLY CREATED TAB:
+  updateTabsetPanel(session, "tabs", shinyInput("new_tab", rv$counter))
+}, ignoreInit = TRUE)
+
 observeEvent(input$add, {
   appendTab(inputId = "tabs",
-            tabPanel("tab_3", 
-                     "This a dynamically-added tab",
-                     value = "tab_1")
+            tabPanel(title = paste("Tab", rv$counter),
+                     value = shinyInput("new_tab", rv$counter),
+                     
+                     ## CONTENT PANEL ----- :
+                     tags$h4(paste("This is tab:", rv$counter)),
+                     p("Remove this tab"),
+                     actionButton(shinyInput("remove_btn", rv$counter), "Remove", icon = icon("minus-circle"))
+            )
   )
 })
 
+## REACTIVITY TO ARRANGE TAB NAMES:
+current.tab <- eventReactive(input$tabs, {
+  # don't accidentally remove main tab:
+  if (!identical(input$tabs, "main")) {
+    input$tabs
+  } else {
+    NULL
+  }
+})
 
-observeEvent(input$remove, {
-  removeTab(inputId = "tabs", target = "tab_3")
+## OBSERVERS FOR THE REMOVE BTNS:
+observe({
+  if (rv$counter > 0L) {
+    lapply(seq(rv$counter), function(x) {
+      observeEvent(input[[paste("remove_btn", x, sep = "_")]], {
+        removeTab(inputId = "tabs", target = current.tab())
+      })
+    })
+  }
 })
