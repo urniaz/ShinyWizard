401b106e501513b8d9ff91cb1e57514278ed4009
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-23 13:12:21 +0200

    update

---
+++ inst/source/tabs/tab4/server.R
@@ -0,0 +1,11 @@
+# Our dataset
+data <- mtcars
+
+output$downloadData <- downloadHandler(
+  filename = function() {
+    paste("data-", Sys.Date(), ".csv", sep="")
+  },
+  content = function(file) {
+    write.csv(data, file)
+  }
+)
---
+++ inst/source/tabs/tab4/ui.R
@@ -0,0 +1,18 @@
+# --- Navigation ---
+# TabName: Download
+# TabIcon: file
+
+# --- Next/Prev buttons
+# ShowButtons: TRUE
+# ButtonsAlignment: center
+
+# --- Info ---
+# InfoTitle: tir
+# InfoMessage: mes
+
+
+fluidPage(
+  fluidRow(
+    downloadButton(ns("downloadData"), "Download")
+  )
+)
