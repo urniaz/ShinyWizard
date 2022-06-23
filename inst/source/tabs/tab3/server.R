5fab960e64f4c1136a579ad0370dffbc9109bc04
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-23 02:33:56 +0200

    d

--- inst/source/tabs/tab3/server.R
+++ inst/source/tabs/tab3/server.R
@@ -3,7 +3,7 @@ rv <- reactiveValues(counter = 0L)
 observeEvent(input$add, {
   rv$counter <- rv$counter + 1L
   appendTab(inputId = "tabs",
-            tabPanel("Dynamic", paste("This a dynamically-added tab", rv))
+            tabPanel("Dynamic", paste("This a dynamically-added tab", rv$counter))
   )
 })
 observeEvent(input$remove, {
