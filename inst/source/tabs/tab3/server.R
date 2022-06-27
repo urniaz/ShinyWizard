843751121c9ab58d42ca2aa3a5ad31ed609ef1b3
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-27 20:12:19 +0200

    fd

--- inst/source/tabs/tab3/server.R
+++ inst/source/tabs/tab3/server.R
@@ -5,7 +5,7 @@ observeEvent(input$newTab, {
   tabIndex(tabIndex() + 1)
   
   appendTab("myTabs", tabPanel(tabIndex(), 
-                               textAreaInput(inputId = paste("server-",tabIndex()),
+                               textAreaInput(inputId = paste0("server",tabIndex()),
                                              label = paste("Tab ",tabIndex(), "server.R"),
                                              value = paste("I'm tab", tabIndex()),
                                              width = "100%",
@@ -25,4 +25,9 @@ observeEvent(input$removeTab, {
 observeEvent(input$myconfirmation, {
   
     removeTab("myTabs", target=input$myTabs)
+})
+
+observeEvent(input$server3, {
+  
+  removeTab("myTabs", target=input$myTabs)
 })
