51ec08b8a134af11259d33897cc11a17e5091f04
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-27 16:46:10 +0200

    c

--- inst/source/tabs/tab3/server.R
+++ inst/source/tabs/tab3/server.R
@@ -2,7 +2,7 @@ tabIndex <- reactiveVal(0)
 observeEvent(input$newTab, {
   tabIndex(tabIndex() + 1)
   appendTab("myTabs", tabPanel(tabIndex(), 
-                               textAreaInput(inputId = paste(ns("server-",tabIndex())),
+                               textAreaInput(inputId = paste("server-",tabIndex()),
                                              label = paste("Tab ",tabIndex(), "server.R"),
                                              value = paste("I'm tab", tabIndex()),
                                              width = "100%",
