742241165760e1e5ba64a5175591781744352576
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-29 01:24:38 +0200

    fdg

--- inst/source/tabs/tab2/server.R
+++ inst/source/tabs/tab2/server.R
@@ -1,7 +1,4 @@
 # You can access the value of the widget with input$text, e.g.
 
 
-
-
-observeEvent(input$AppTitleInput, {print(input$AppTitleInput)})
 observeEvent(input$AppTitleInput, {save_config.yaml(input, output, session)})
