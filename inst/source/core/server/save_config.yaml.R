0a286b72c492d8ab800127c373d305387e2b2758
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-29 01:36:35 +0200

    vcxv

--- inst/source/core/server/save_config.yaml.R
+++ inst/source/core/server/save_config.yaml.R
@@ -6,10 +6,10 @@ save_config.yaml <- function(input, output, session){
   #  - Load configuration file original
   temp_config <- yaml::yaml.load_file(system.file("source/config.yaml", package = "ShinyWizard"))
   #  - Edit original variables
-  temp_config$AppTitle <-  {print(input$AppTitleInput)}
-  temp_config$AppSubTitle <-  {print(input$AppSubTitle)}
+  config$AppTitle <-  {print(input$AppTitleInput)}
+  config$AppSubTitle <-  {print(input$AppSubTitle)}
 
- print(temp_config)
+ print(config)
 }
 
 
