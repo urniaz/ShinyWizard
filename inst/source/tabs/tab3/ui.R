60fbdd2d7c904301c2b75a7d7c754d7eea03565b
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-28 02:18:15 +0200

    y

--- inst/source/tabs/tab3/ui.R
+++ inst/source/tabs/tab3/ui.R
@@ -15,7 +15,7 @@ fluidPage(
   fluidRow(
     actionButton(ns("newTab"), "Append tab", icon = icon("plus")),
     actionButton(ns("removeTab"), "Remove current tab", icon = icon("minus")),
-    actionButton(ns("runTabs"), "Run tabs", icon = icon("minus"))
+    actionButton(ns("runTabs"), "Run tabs", icon = icon("play"))
   ),
   tags$br(),
   tabsetPanel(id=ns("EditTabs"), type="pills")
