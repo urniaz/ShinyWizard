4d9a6f202d35615876ee7482c8b538c40cf27092
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-06-29 14:42:59 +0200

    dd

--- inst/source/tabs/tab2/server.R
+++ inst/source/tabs/tab2/server.R
@@ -4,8 +4,8 @@
 observeEvent(input$AppTitleInput, { save_config.yaml(input, output, session)}) 
 observeEvent(input$AppSubTitleInput, { save_config.yaml(input, output, session)}) 
 observeEvent(input$LogoFilePathInput, { save_config.yaml(input, output, session)}) 
-observeEvent(input$LogoFilePathInput, { save_config.yaml(input, output, session)}) 
-observeEvent(input$LogoFilePathInput, { save_config.yaml(input, output, session)}) 
+observeEvent(input$FooterTextInput, { save_config.yaml(input, output, session)}) 
+observeEvent(input$shinytheme-selector, { save_config.yaml(input, output, session)}) 
 
 
 
