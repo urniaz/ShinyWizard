20ceff5ef7e7989f5649ae046efff2d643c74197
RafalUrniaz <17867916+RafalUrniaz@users.noreply.github.com>
2022-07-01 01:36:32 +0200

    dfdsf

--- inst/source/tabs/tab3/server.R
+++ inst/source/tabs/tab3/server.R
@@ -19,14 +19,14 @@ observeEvent(input$newTab, {
                                textAreaInput(inputId = paste0("tab3-ui",tabIndex()),
                                              label = paste("Tab ",tabIndex(), "ui.R"),
                                              value = paste("# --- Navigation ---
-                                                            # TabName: Tab", tabIndex(),
-                                                            "# TabIcon: file
-                                                            # --- Next/Prev buttons
-                                                            # ShowButtons: TRUE
-                                                            # ButtonsAlignment: center
-                                                            # --- Info ---
-                                                            # InfoTitle: Tab", tabIndex(),
-                                                           "# InfoMessage: Info about tab"),
+# TabName: Tab", tabIndex(),
+"# TabIcon: file
+# --- Next/Prev buttons
+# ShowButtons: TRUE
+# ButtonsAlignment: center
+# --- Info ---
+# InfoTitle: Tab", tabIndex(),
+"# InfoMessage: Info about tab"),
                                              width = "50%",
                                              height = "100%",
                                              resize = "both")
