c0540be5011cab9106132b882662c5a4ef6b15b5
“rafal.urn” <4932398-rafal.urn@users.noreply.gitlab.com>
2024-08-09 22:13:47 +0200

    upadate

--- inst/source/tabs/tab3/server.R
+++ inst/source/tabs/tab3/server.R
@@ -71,10 +71,12 @@ if (tabIndex() >=1){
      
      
     }
-  shinyjs::alert("ok!1")
+  
   # Run project
   write_file(paste0('ShinyWizard::RunShinyWizard(loc ="../',TempProjPath,'")'), paste0(TempProjPath, "run.R"))
-  callr::rscript(paste0(TempProjPath, "run.R"))
+  
+  rstudioapi::jobRunScript(paste0(TempProjPath, "run.R"))
+  # callr::rscript(paste0(TempProjPath, "run.R"))
   shinyjs::alert("ok!")
 }
 })
