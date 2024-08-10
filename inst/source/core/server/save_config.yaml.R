519e1e49440c31d989d99694ee9c414f398bf024
“rafal.urn” <4932398-rafal.urn@users.noreply.gitlab.com>
2024-08-10 02:48:13 +0200

    fixwed

--- inst/source/core/server/save_config.yaml.R
+++ inst/source/core/server/save_config.yaml.R
@@ -16,7 +16,7 @@ save_config.yaml <- function(input, output, session){
   
   TempProjPath <- paste0(tempdir(),"/SWTempProj/")
   dir.create(TempProjPath, showWarnings = FALSE)
-  cat(temp_config)
+  summary(temp_config)
   yaml::write_yaml(temp_config, paste0(TempProjPath,"config.yaml"))
    
 }
