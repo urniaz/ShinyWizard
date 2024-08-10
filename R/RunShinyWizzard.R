fdb22a3290c37c5be44d3dd35d92513d28112a4f
“rafal.urn” <4932398-rafal.urn@users.noreply.gitlab.com>
2024-08-10 15:14:00 +0200

    upadate

--- R/RunShinyWizzard.R
+++ R/RunShinyWizzard.R
@@ -21,37 +21,16 @@ RunShinyWizard <- function(loc = NULL){
   }else{
     
     # Copy all original files
-    file.copy(system.file("source", package = "ShinyWizard"), loc, recursive=TRUE)
-    
-    # Dir loc exists 
-    if (dir.exists(loc)){
-    #    # check tabs and config exist
-    #    if (dir.exists(paste0(loc, "/tabs/")) && file.exists(paste0(loc, "/config.yaml"))){
-    #    # Clear demo data & copy tabs
-    #    unlink(paste0(TempPath, "/source/tabs/"), recursive = TRUE)
-    #    dir.create(paste0(TempPath,"/source/tabs/"), showWarnings = FALSE)
-    #    file.copy(paste0(loc, "/tabs/"), paste0(TempPath, "/source/"), recursive=TRUE)
-    #    # Clear demo config & copy config.yaml
-    #    unlink(paste0(TempPath, "/source/config.yaml"), recursive = TRUE)
-    #    file.copy(paste0(loc, "/config.yaml"), paste0(TempPath, "/source/"), recursive=TRUE)
-    #    }else{ cat("The directory should contain at least 'tabs' folder and config.yaml file!")}
-    #    #if (dir.exists(paste0(loc, "/www/"))){
-    #    
-    #    # Copy www and overwrite 
-    # #   unlink(paste0(TempPath, "/source/www/"), recursive = TRUE)
-    # #   file.copy(paste0(loc, "/www/"), paste0(TempPath, "/source/"), recursive=TRUE, overwrite = TRUE)
-    #    
-    #  # }else{cat("The directory www ", loc ," does not exists!")}
-    #  }
-    }
+    file.copy(system.file("source/core", package = "ShinyWizard"), loc, recursive=TRUE)
+    file.copy(system.file("source/global.R", package = "ShinyWizard"), loc, recursive=TRUE)
+    file.copy(system.file("source/server.R", package = "ShinyWizard"), loc, recursive=TRUE)
+    file.copy(system.file("source/ui.R", package = "ShinyWizard"), loc, recursive=TRUE)
+    file.copy(system.file("source/www", package = "ShinyWizard"), loc, recursive=TRUE)
     
     
     # Run project
-    write_file(paste0("shiny::runApp('",paste0(loc, "/source/"),"', launch.browser = TRUE)"), paste0(loc, "/run.R"))
-    
+    write_file(paste0("shiny::runApp('", loc,"', launch.browser = TRUE)"), paste0(loc, "/run.R"))
     rstudioapi::jobRunScript(paste0(loc, "/run.R"))
-    
-    
     
   }
   
