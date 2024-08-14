53d9e5a0974925f588e35829b8e03f05d7edd837
“rafal.urn” <4932398-rafal.urn@users.noreply.gitlab.com>
2024-08-15 01:34:43 +0200

    d

--- inst/source/tabs/tab3/ui.R
+++ inst/source/tabs/tab3/ui.R
@@ -13,6 +13,12 @@
 
 fluidPage(
   fluidRow(
+    selectInput(
+      "select",
+      "Select option",
+      choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
+      selected = 1
+    ),
     actionButton(ns("newTab"), "Append tab", icon = icon("plus")),
     actionButton(ns("removeTab"), "Remove current tab", icon = icon("minus")),
     actionButton(ns("runTabs"), "Run tabs", icon = icon("play"))
