666bc8377181589460b049c374529e1a86138779
“rafal.urn” <4932398-rafal.urn@users.noreply.gitlab.com>
2024-08-16 15:57:48 +0200

    aa

--- inst/source/tabs/tab4/server.R
+++ inst/source/tabs/tab4/server.R
@@ -1,14 +1,14 @@
 
 
 # Download Shiny Wizard ZIP file
-# output$DownloadShinyWizardZIP <- downloadHandler(
-#   filename = function() {
-#     paste("ShinyWizard-", Sys.Date(), ".zip", sep="")
-#   },
-#   content = function(file) {
-#     utils::zip(data, list.files())
-#   }
-# )
+output$DownloadShinyWizardZIP <- downloadHandler(
+  filename = function() {
+    paste("ShinyWizard-", Sys.Date(), ".zip", sep="")
+  },
+  content = function() {
+    utils::zip(tempfile(), list.files(config$TempProjPath))
+  }
+)
 
 
 output$result <- renderText({(paste("config", tempdir(), " /+/ ", config$TempProjPath))})
