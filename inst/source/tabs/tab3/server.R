e8870479d2cede209ae4ada57ff866d710b068a3
“rafal.urn” <4932398-rafal.urn@users.noreply.gitlab.com>
2024-08-16 17:17:41 +0200

    del func

--- inst/source/tabs/tab3/server.R
+++ inst/source/tabs/tab3/server.R
@@ -58,6 +58,7 @@ observeEvent(input$removeTab, {
 })
 
 observeEvent(input$DelTabConfirmation, {
+    unlink(paste0(config$TempProjPath, "tabs/tab", input$EditTabs), recursive=TRUE)
     removeTab("EditTabs", target=input$EditTabs)
 })
 # <---
