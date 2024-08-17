acc19f0b125b185eb100f54ff805d67cf10878d6
“rafal.urn” <4932398-rafal.urn@users.noreply.gitlab.com>
2024-08-18 00:52:02 +0200

    dfs

--- inst/source/tabs/tab4/ui.R
+++ inst/source/tabs/tab4/ui.R
@@ -24,19 +24,19 @@ fluidPage(
     layout_columns(
       col_width = 3,
       card(
-        card_header(HTML("<h3><b>Running in terminal</b></h3><br /><br />")),
+        card_header(HTML("<h3><b>Running in terminal</b></h3><br />")),
         HTML(
           '<br />Open text editor, write command <code> ShinyWizard::RunShinyWizard(&#60;Path to downloaded file&#62;)</code><br /> save file as <code>run.R</code> and run from console by command<br /> <code>Rscript run.R</code>'
         )
       ),
       card(
-        card_header(HTML("<h3><b>Running in R studio console</b></h3><br /><br />")),
+        card_header(HTML("<h3><b>Running in R studio console</b></h3><br />")),
         HTML(
           'Open R studio, write command to console <br /><br /><code> ShinyWizard::RunShinyWizard(&#60;Path to downloaded file&#62;)</code><br /><br /> and click enter.'
         )
       ),
       card(
-        card_header(HTML("<h3><b>Adding to your package</b></h3><br /><br />")),
+        card_header(HTML("<h3><b>Adding to your package</b></h3><br />")),
         HTML(
           'Copy downloaded zip file to the <code>inst/</code> folder of your package and add the command to your package<br /> <br /><code>ShinyWizard::RunShinyWizard(paste0(system.file(package = "&#60;Your package name&#62;"), &#60;downloaded file name&#62;)</code>'
         )
