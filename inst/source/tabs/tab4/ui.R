26c31efd69fe4d182771619f83b8fad6b959c64e
“rafal.urn” <4932398-rafal.urn@users.noreply.gitlab.com>
2024-08-18 00:39:41 +0200

    sdasd

--- inst/source/tabs/tab4/ui.R
+++ inst/source/tabs/tab4/ui.R
@@ -24,26 +24,26 @@ fluidPage(
     layout_columns(
       col_width = 3,
       card(
-        card_header(HTML("<b>Running in terminal</b>")),
+        card_header(HTML("<b>Running in terminal</b><br /><br />")),
         HTML(
           '<br />Open text editor, write command <code> ShinyWizard::RunShinyWizard(&#60;Path to downloaded file&#62;)</code><br /> save file as <code>run.R</code> and run from console by command<br /> <code>Rscript run.R</code>'
         )
       ),
       card(
-        card_header(HTML("<b>Running in R studio console</b>")),
+        card_header(HTML("<b>Running in R studio console</b><br /><br />")),
         HTML(
           'Open R studio, write command to console <br /><br /><code> ShinyWizard::RunShinyWizard(&#60;Path to downloaded file&#62;)</code><br /><br /> and click enter.'
         )
       ),
       card(
-        card_header("Adding to your package"),
+        card_header(HTML("<b>Adding to your package</b><br /><br />")),
         HTML(
           'Copy downloaded zip file to the <code>inst/</code> folder of your package and add the command to your package<br /> <br /><code>ShinyWizard::RunShinyWizard(paste0(system.file(package = "&#60;Your package name&#62;"), &#60;downloaded file name&#62;)</code>'
         )
       )
   )
-  ),
+  )
   
-  verbatimTextOutput(outputId = ns("result")),
+  # ,verbatimTextOutput(outputId = ns("result")),
 
 )
