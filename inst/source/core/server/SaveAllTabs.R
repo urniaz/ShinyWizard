5e40bc3fa9751fe08af36ce8b7af82bb7cbee493
“rafal.urn” <4932398-rafal.urn@users.noreply.gitlab.com>
2024-08-17 11:15:48 +0200

    SaveAllTabs <- function(input, output, session){

---
+++ inst/source/core/server/SaveAllTabs.R
@@ -0,0 +1,18 @@
+#' Save all tabs
+
+SaveAllTabs <- function(input, output, session){
+  
+  # Temp dir tabs clear and save current
+  unlink(config$TempProjPathTabs, recursive = TRUE, force = TRUE)
+  dir.create(config$TempProjPathTabs, showWarnings = FALSE)
+  
+  for (i in currentTabIndexes()){
+    
+    try({
+      tabs_dir_n <- paste0(config$TempProjPathTabs,"tab", i,"/")
+      dir.create(tabs_dir_n, showWarnings = FALSE)
+      write_file(isolate(input[[paste0("ui",i)]]), paste0(tabs_dir_n, "/ui.R"))
+      write_file(isolate(input[[paste0("server",i)]]),paste0(tabs_dir_n, "/server.R"))
+    })
+  }
+}
--- inst/source/tabs/tab3/server.R
+++ inst/source/tabs/tab3/server.R
@@ -87,28 +87,15 @@ observeEvent(input$DelTabConfirmation, {
 # Run project
 observeEvent(input$runTabs, {
   
-if (tabIndex() >=1){
+#if (tabIndex() >=1){
  
-  # Temp dir tabs clear and save current
-  unlink(config$TempProjPathTabs, recursive = TRUE, force = TRUE)
-  dir.create(config$TempProjPathTabs, showWarnings = FALSE)
-  
-   for (i in currentTabIndexes()){
-   
-     try({
-       tabs_dir_n <- paste0(config$TempProjPathTabs,"tab", i,"/")
-       dir.create(tabs_dir_n, showWarnings = FALSE)
-       write_file(isolate(input[[paste0("ui",i)]]), paste0(tabs_dir_n, "/ui.R"))
-       write_file(isolate(input[[paste0("server",i)]]),paste0(tabs_dir_n, "/server.R"))
-        })
-     
-    }
-  
+  # Save all tabs
+  SaveAllTabs(input, output, session)
+    
   # Run project
-  
   ShinyWizard::RunShinyWizard(loc = config$TempProjPath)
 
-}
+#}
 })
 
 output$result <- renderText({(paste(currentTabIndexes()))})
