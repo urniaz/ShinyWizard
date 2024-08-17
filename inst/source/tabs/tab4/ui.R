ba8c87a7451ad4267fc207c6a2fe7ee05fb988b4
“rafal.urn” <4932398-rafal.urn@users.noreply.gitlab.com>
2024-08-17 23:58:34 +0200

    up

--- inst/source/tabs/tab4/ui.R
+++ inst/source/tabs/tab4/ui.R
@@ -14,34 +14,37 @@
 fluidPage(
   fluidRow(HTML('<center><h1>Download & Deploy Your Project !</h1>
       <p> Now it is time for download and deploy your project.
-                Remember to save changes and download file.
-                <br /> <br />
+      <br /> <br />
       </p>
       '),
-    downloadButton(ns("DownloadShinyWizardZIP"), "Download")
+           
+    downloadButton(ns("DownloadShinyWizardZIP"), "Download"),
+    
+    HTML('Downloaded file can be simply deployed by,... '),
+    
+    layout_columns(
+      col_width = 3,
+      card(
+        card_header("Running in terminal"),
+        HTML(
+          'Open text editor, write command <br /><br /><code> ShinyWizard::RunShinyWizard(&#60;Path to downloaded zip file&#62;)</code><br /><br /> save file as <code>run.R</code> and run from console by command<br /><br /><code>Rscript run.R</code>'
+        )
+      ),
+      card(
+        card_header("Running in R studio console"),
+        HTML(
+          'Open R studio, write command to console <br /><br /><code> ShinyWizard::RunShinyWizard(&#60;Path to downloaded zip file&#62;)</code><br /><br /> and click enter.'
+        )
+      ),
+      card(
+        card_header("Adding to your package"),
+        HTML(
+          'Copy downloaded zip file to the <code>inst/</code> folder of your package and add the command to your package<br /> <br /><code>ShinyWizard::RunShinyWizard(paste0(system.file(package = "&#60;name of your package&#62;"), &#60;downloaded zip filename&#62;)</code>'
+        )
+      )
+  )
   ),
-  HTML('<div class="row">
-  <div class="col-sm-6 mb-3 mb-sm-0">
-    <div class="card border-light mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Light card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-  </div>
-  </div>
-         
-  </div>
-  <div class="col-sm-6">
-    <div class="card border-light mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Light card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>
-         </div>
-         </div>
-         
-  </div>
-</div>'),
+  
   HTML('<center>
       <p>      <br /> <br />
                 Downloaded file can be simply deployed as,... 
