74a91c76fc1774fe280134a3cfd86b0d82549390
“rafal.urn” <4932398-rafal.urn@users.noreply.gitlab.com>
2024-08-17 23:17:12 +0200

    aa

--- inst/source/tabs/tab4/ui.R
+++ inst/source/tabs/tab4/ui.R
@@ -22,22 +22,24 @@ fluidPage(
   ),
   HTML('<div class="row">
   <div class="col-sm-6 mb-3 mb-sm-0">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
+    <div class="card border-light mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Light card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+  </div>
+  </div>
+         
   </div>
   <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
+    <div class="card border-light mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Light card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>
+         </div>
+         </div>
+         
   </div>
 </div>'),
   HTML('<center>
