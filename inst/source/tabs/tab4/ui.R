2208d989d163fdc4c60567bd2225af3d73b2dae4
“rafal.urn” <4932398-rafal.urn@users.noreply.gitlab.com>
2024-08-17 12:32:54 +0200

    <br />

--- inst/source/tabs/tab4/ui.R
+++ inst/source/tabs/tab4/ui.R
@@ -12,11 +12,23 @@
 
 
 fluidPage(
-  fluidRow(
+  fluidRow(HTML('<center><h1>Welcome!</h1>
+      <p> Now it is time for download and deploy your project.
+                Remember to save changes and download file.
+                <br /> <br />
+                Downloaded file can be simply deployed as,... 
+                <br /> <br /> separate file <br /> 
+                <code>ShinyWizard::RunShinyWizard()</code>
+                  <br /> <br /> 
+                  or a part of your package 
+                  <br /> 
+                <code>ShinyWizard::RunShinyWizard()</code>
+      </p>
+      '),
     downloadButton(ns("DownloadShinyWizardZIP"), "Download")
   ),
   tags$br(),
-  
+  HTML('<center>'),
   verbatimTextOutput(outputId = ns("result")),
 
 )
